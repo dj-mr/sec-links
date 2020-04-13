53bfefbc4097f10c46fe7c625feccf10ff67eb8d
D. Jagan <mr.daruri@outlook.com>
2020-04-13 12:38:23 -0700

    Initial Commit

---
+++ src/main/java/com/probemore/FinancialDataApplication.java
@@ -0,0 +1,13 @@
+package com.probemore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/probemore/FinancialDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probemore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
