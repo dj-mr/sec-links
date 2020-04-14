442607390a2c0b1f297301a1e65caf966b5e90eb
D. Jagan <mr.daruri@outlook.com>
2020-04-13 19:01:47 -0700

    Updated Swagger documentation for GET method on this class

--- src/main/java/com/probemore/controller/SECLinks.java
+++ src/main/java/com/probemore/controller/SECLinks.java
@@ -4,6 +4,10 @@
 package com.probemore.controller;
 
 import com.probemore.model.SECData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/sec",
                 produces = "application/json")
+@Tag(name = "SECLinks", description = "SEC Links API")
 @CrossOrigin(origins = "*")
 public class SECLinks {
 
@@ -19,6 +24,13 @@ public class SECLinks {
      * Method that handles GET request for this controller.
      * @return Available SEC data
      */
+    @Operation(summary = "GET all SEC Links",
+               description = "Get all SEC Links stored in local database",
+               tags = {"sec"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Return of data"),
+            @ApiResponse(responseCode = "204", description = "Content was not found")
+    })
     @GetMapping
     public Iterable<SECData> secLinks() {
         // TODO - Add Business Logic here
