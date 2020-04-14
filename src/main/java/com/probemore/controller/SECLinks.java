8fe7560bfaadd891b4f2b9619d8c6c8854b99bbb
D. Jagan <mr.daruri@outlook.com>
2020-04-13 19:31:38 -0700

    Swagger Links work for SEC GET and PUT

--- src/main/java/com/probemore/controller/SECLinks.java
+++ src/main/java/com/probemore/controller/SECLinks.java
@@ -8,10 +8,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/sec",
@@ -32,9 +31,25 @@ public class SECLinks {
             @ApiResponse(responseCode = "204", description = "Content was not found")
     })
     @GetMapping
-    public Iterable<SECData> secLinks() {
+    public List<SECData> getAll() {
         // TODO - Add Business Logic here
         return null;
+    }
+
+
+    /**
+     * Method that handles PUT request for this controller.
+     */
+    @Operation(summary = "Refresh SEC links data",
+               description = "Refresh SEC Links from SEC database",
+               tags = {"sec"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful refresh operation"),
+            @ApiResponse(responseCode = "401", description = "User not authorized to perform request")
+    })
+    @PutMapping
+    public void refreshDatabase() {
+        // TODO - Add Business Logic here
     }
 
 }
